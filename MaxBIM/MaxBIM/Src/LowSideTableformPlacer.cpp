6219fe249bf7f5ca78d80461ad266e8ff6fce7f6
Soonbum Jeong <peacemaker84@gmail.com>
2022-05-23 08:49:44 +0900

    MaxBIM Add-on (2022.05.23 - 08:46)
    수정됨
    1. 낮은 슬래브 측면에 테이블폼 배치
      - 층별 고도값 반영

--- MaxBIM/MaxBIM/Src/LowSideTableformPlacer.cpp
+++ MaxBIM/MaxBIM/Src/LowSideTableformPlacer.cpp
@@ -320,10 +320,10 @@ GSErrCode	placeTableformOnLowSide (void)
 	placingZone.verLen = infoMorph.verLen;
 	placingZone.ang = infoMorph.ang;
 
-	if (nWall == 1)			placingZone.leftBottomZ = infoWall.bottomOffset;
-	else if (nBeam == 1)	placingZone.leftBottomZ = infoBeam.level - infoBeam.height;
-	else if (nSlab == 1)	placingZone.leftBottomZ = infoSlab.level + infoSlab.offsetFromTop - infoSlab.thickness;
-	else if (nMesh == 1)	placingZone.leftBottomZ = infoMesh.level - infoMesh.skirtLevel;
+	if (nWall == 1)			placingZone.leftBottomZ = infoWall.bottomOffset + workLevel_structural;
+	else if (nBeam == 1)	placingZone.leftBottomZ = infoBeam.level - infoBeam.height + workLevel_structural;
+	else if (nSlab == 1)	placingZone.leftBottomZ = infoSlab.level + infoSlab.offsetFromTop - infoSlab.thickness + workLevel_structural;
+	else if (nMesh == 1)	placingZone.leftBottomZ = infoMesh.level - infoMesh.skirtLevel + workLevel_structural;
 
 	// 테이블폼 방향 결정 (600mm 미만이면 가로, 이상이면 세로)
 	if (placingZone.verLen < 0.600 - EPS) {
