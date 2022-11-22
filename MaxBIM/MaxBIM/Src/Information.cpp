ad9afd907edeceee84e204b499f8cf05f3cc6962
Soonbum Jeong <peacemaker84@gmail.com>
2022-11-22 10:01:22 +0900

    MaxBIM Add-on (2022.11.22 - 09:58)
    수정됨
    1. 애드온 유효성 문제: 재빌드 실시

--- MaxBIM/MaxBIM/Src/Information.cpp
+++ MaxBIM/MaxBIM/Src/Information.cpp
@@ -255,7 +255,7 @@ short DGCALLBACK aboutHandler (short message, short dialogID, short item, DGUser
 			// 라벨: 버전 (최근 배포일)
 			itmIdx = DGAppendDialogItem (dialogID, DG_ITM_STATICTEXT, DG_IS_LEFT, DG_FT_NONE, itmPosX, itmPosY, 250, 23);
 			DGSetItemFont (dialogID, itmIdx, DG_IS_LARGE | DG_IS_PLAIN);
-			DGSetItemText (dialogID, itmIdx, L"배포일: 2022.11.21 - 17:25");
+			DGSetItemText (dialogID, itmIdx, L"배포일: 2022.11.22 - 09:58");
 			DGShowItem (dialogID, itmIdx);
 			itmPosY += 30;
 
